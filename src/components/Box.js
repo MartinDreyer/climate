0ee20fc6d4546ff4f74fc0c626ebb0a3113b2c8e
Martin Dreyer <martinchristiandreyer@gmail.com>
2023-08-17 12:49:05 +0200

    mobile

--- src/components/Box.js
+++ src/components/Box.js
@@ -17,13 +17,16 @@ export default function Box({ size, color, id, name }) {
 
 
   return (
-    <div
-      style={style}
-      id={id}
-      className={`flex justify-center items-center box`}>
-      <p className="hide-on-mobile">
-        {name}
-      </p>
-    </div>
+    <>
+      <p className="hide-on-desktop"><b style={{color: color, fontSize:'12px'}}>{name}</b></p>
+      <div
+        style={style}
+        id={id}
+        className={`flex justify-center items-center box`}>
+        <p className="hide-on-mobile">
+          {name}
+        </p>
+      </div>
+    </>
   )
 }
